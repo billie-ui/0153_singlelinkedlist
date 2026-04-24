2dc796638ef20fe06316f20ba43f7ffd495d175c
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-04-24 15:31:21 +0700

    tambah fungsi addNode dengan validasi duplikasi

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -7,4 +7,29 @@ class Node
         int noMhs;
         Node *next;
 };
+class linkedlist
+{
+    Node *START;
+
+    public:
+        linkedlist()
+        {
+            START = NULL;
+        }
+
+        void addNode()
+        {
+            int nim;
+            cout << "\nMasukkan Nomor Mahasiswa : ";
+            cin >> nim;
+
+            Node *nodeBaru = new Node;
+            nodeBaru->noMhs = nim;
+
+            if (START == NULL || nim <= START->noMhs)
+            {
+                if ((START != NULL) && (nim == START->noMhs))
+                {
+                    cout << "\nDuplikasi noMhs tidak diizinkan\n";
+                    return;
 
